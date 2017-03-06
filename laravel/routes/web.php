6db7b7ef6fbd599bec27d1da4f4cb38e7ebf0ec5
Mert Ocak <mertocak@gmail.com>
2017-03-06 15:27:22 +0300

    Controllers

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,63 +11,6 @@
 |
 */
 
-use App\Task;
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/',function(){
-
-	$tasks = DB::table('tasks')->latest()->get();
-
-	return view('welcome',compact('tasks'));
-
-});
-
-Route::get('/tasks', function () {
-
-	// $tasks = DB::table('tasks')->get();
-
-	// $tasks = DB::table('tasks')->where('created_at','<','2017-02-26 12:38:58')->get();
-
-	// $tasks = DB::table('tasks')->latest()->get();
-
-	$tasks = Task::all();
-
-	return view('tasks.index',compact('tasks'));
-
-}
-
-);
-
-Route::get('/tasks/{task}',function($id){
-
-	// dd($id);
-
-	// $tasks = DB::table('tasks')->latest()->get();
-
-	// $task = DB::table('tasks')->find($id);
-
-	// dd($task);
-
-	$task = Task::find($id);
-
-	return view('tasks.show',compact('task'));
-
-
-});
-
-Route::get('about',function(){
-
-	$tasks = [
-
-	'Turn off the lights',
-
-	'Go to the supermarket',
-
-	'Watch \' John Wick Chapter 2\' ',
-
-	'Wash the car'
-
-	];
-
-	return view('about')->with('tasks',$tasks);
-
-});
+Route::get('/tasks/{task}','TasksController@show');
